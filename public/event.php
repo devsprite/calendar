3e9f596767ee7e1f3506479a35977c9e5a66b751
Dominique <dominique@chez-dominique.fr>
2018-04-08 21:51:11 +0200

    add view event

--- public/event.php
+++ public/event.php
@@ -1,13 +1,35 @@
 <?php
 
 use Calendar\Events;
-require('../src/Calendar/Events.php');
 
-$events = new Events();
+require('../src/bootstrap.php');
 
-require ('../views/header.php');
+
+if (!isset($_GET['id'])) {
+    e404();
+}
+
+$pdo = get_pdo();
+$events = new Events($pdo);
+try {
+    $event = $events->find($_GET['id']);
+} catch (Exception $e) {
+    e404();
+}
+
+render('header', ['title' => $event->getName()]);
 
 ?>
+<h1><?= h($event->getName()) ?></h1>
 
+<ul>
+    <li>Date: <?= $event->getStart()->format('d/m/Y'); ?></li>
+    <li>Heure de démarrage: <?= $event->getStart()->format('H:i'); ?></li>
+    <li>Heure de fin: <?= $event->getEnd()->format('H:i'); ?></li>
+    <li>
+        Description: <br>
+        <?= h($event->getDescription()); ?>
+    </li>
+</ul>
 
-<?php require ('../views/footer.php'); ?>
+<?php require('../views/footer.php'); ?>
