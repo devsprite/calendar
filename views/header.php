69cf9b7458dea1252129bc198c5c24cc0a912b16
Dominique <dominique@chez-dominique.fr>
2018-04-10 07:34:45 +0200

    install pwa

--- views/header.php
+++ views/header.php
@@ -5,10 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/calendar.css">
+    <link rel="stylesheet" href="manifest.json">
     <title><?= (isset($title)) ? h($title) : 'Mon Calendrier'; ?></title>
+    <script>;
+        if('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('/sw.js')
+                .then(function() { console.log("Service Worker Registered"); });
+        }
+    </script>;
 </head>
 <body>
 
