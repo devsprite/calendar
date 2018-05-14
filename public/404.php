439d696760e4a6fcf79e3fcd45910d1135c343fe
Dominique <devsprite@gmail.com>
2018-05-14 21:55:29 +0200

    feat(calendar): display 404

--- public/404.php
+++ public/404.php
@@ -2,5 +2,11 @@
 http_response_code(404);
 require ('../views/header.php');
 ?>
+<<<<<<< HEAD
 <h1>Cette page n'existe pas.</h1>
+=======
+
+<h1>Cette page n'existe pas.</h1>
+
+>>>>>>> 044bb84... change one file
 <?php require ('../views/footer.php') ?>
